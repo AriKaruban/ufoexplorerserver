96395288fd16345d4ae0fa46dac8506fb1f91827
Oneknowall <acalvin2@kent.edu>
2021-02-17 14:47:02 -0500

    test query again

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/1980_1999/:id/:field',(req,res)=>{
 })
 app.get('/:year',(req,res)=>{
     var year = req.params.year;
-    let tempjson = []
+    var tempjson = []
     if(year>=1980&&year<=1999){
         for(let x=0;x<parsedata.length;x++){
             if(parsedata[x].occuredDate.substr(parsedata[x].occuredDate.length - 4)==year){
