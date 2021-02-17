3eb4ed90fdf5471e3cf27e8e8e8093ce3334b53d
Oneknowall <acalvin2@kent.edu>
2021-02-17 15:25:20 -0500

    test query

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
 const e = require('express');
+const { resolve } = require('path');
 let rawdata = fs.readFileSync('1980-1999.json');
 let parsedata = JSON.parse(rawdata);
 let rawdata2 = fs.readFileSync('2010-2021.json');
@@ -24,20 +25,24 @@ app.get('/1980_1999/:id/:field',(req,res)=>{
 })
 async function getdataofyear(year){
     var tempjson = []
-    for(const entry of parsedata){
+    let p=new Promise((resolve,rejected)=>{
+        for(const entry of parsedata){
             console.log("forloop")
             if(entry.occuredDate.substr(entry.occuredDate.length - 4)==year){
                 tempjson.push(entry)
             }
+            resolve(tempjson)
         }
-    return tempjson
+    })
+    let data = await p
+    return data
 }
 app.get('/:year',(req,res)=>{
     var year = req.params.year;
     if(year>=1980&&year<=1999){
         console.log("year in data")
-        let tempjson = await getdataofyear(year)
-        res.send(tempjson)
+        let tempjson = getdataofyear(year)
+        res.send(getdataofyear(year))
     }else{res.send("year not in data")}
 })
 const port=process.env.PORT||3000;
