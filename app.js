9b14dd5a5b452be8d9f1546b5e7035dabfc80116
Oneknowall <acalvin2@kent.edu>
2021-02-17 13:34:22 -0500

    added jsons

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const app=express();
-
+const fs = require('fs');
+let rawdata = fs.readFileSync('1980-1999.json');
+let parsedata = JSON.parse(rawdata);
 app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!')
+    res.send(parsedata)
 })
-
-app.listen(process.env.PORT||3000,()=>{
+const port=process.env.PORT||3000;
+console.log(port);
+app.listen(port,()=>{
     console.log('Listening on localhost');
 })
