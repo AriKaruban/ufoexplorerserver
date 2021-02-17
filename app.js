79ababb03de843cc60bb70cb9c3cb0a7ab78c5c4
Oneknowall <acalvin2@kent.edu>
2021-02-17 13:40:20 -0500

    added data

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app=express();
 const fs = require('fs');
 let rawdata = fs.readFileSync('1980-1999.json');
 let parsedata = JSON.parse(rawdata);
+let rawdata2 = fs.readFileSync('2010-2021.json');
+let parsedata2 = JSON.parse(rawdata2);
 app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
     res.send(parsedata)
+})
+app.get('/2010_2021',(req,res)=>{
+    res.send(parsedata2)
 })
 const port=process.env.PORT||3000;
 console.log(port);
