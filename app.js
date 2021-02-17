005057a7234641ae32c67563d300b5d78a95d013
Oneknowall <acalvin2@kent.edu>
2021-02-17 16:04:24 -0500

    test query again

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ async function getdataofyear(year,res){
     let p=new Promise((resolve,rejected)=>{
         var tempjson = []
         for(const entry of parsedata){
-            console.log("forloop")
             if(entry.occuredDate.substr(entry.occuredDate.length - 4)==year){
                 tempjson.push(entry)
             }
